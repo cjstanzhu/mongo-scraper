66befbbd52fc59f084e62d61f21fc20aec84ed97
cjstanzhu <cjstanzhu@gmail.com>
2019-10-04 23:58:35 -0400

    insert code for deployed database connection

--- server.js
+++ server.js
@@ -22,7 +22,10 @@ let exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-mongoose.connect("mongodb://localhost/mongoscraperdb", { useNewUrlParser: true });
+// If deployed, use the deployed database. Otherwise use the local database
+let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoscraperdb";
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
